819fa446f8f9953b98bc25e59d86642c5e9fbd0e
Agnes Corpuz <catherine_agnes.corpuz@genesys.om>
2023-09-27 15:33:43 +0800

    Use CX as Code for process automation trigger

--- blueprint/src/terraform/trigger_workflow.tf
+++ blueprint/src/terraform/trigger_workflow.tf
@@ -5,14 +5,28 @@ data "genesyscloud_flow" "get_flow" {
   name = "EventOrchestrator Flow"
 }
 
-resource "null_resource" "trigger" {
+resource "genesyscloud_processautomation_trigger" "trigger" {
   depends_on = [
     genesyscloud_flow.event_orchestrator_workflow,
     data.genesyscloud_flow.get_flow
-  ]
+  ] 
 
-  provisioner "local-exec" {
-    command = "python3 workflow_trigger/trigger.py ${data.genesyscloud_flow.get_flow.id}"
+  name       = "Call Disconnected Trigger"
+  topic_name = "v2.detail.events.conversation.{id}.customer.end"
+  enabled    = true
+
+  target {
+    id   = data.genesyscloud_flow.get_flow.id
+    type = "Workflow"
   }
-}
+  
+  match_criteria = jsonencode([
+        {
+            "jsonPath": "mediaType",
+            "operator": "Equal",
+            "value": "VOICE"
+        }
+    ])
 
+  event_ttl_seconds = 60
+} 
